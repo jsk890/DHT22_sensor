fb9566d65efa16bb7ee403d3c451054d7ace9fdd
jsk890 <resqme89@naver.com>
2022-06-22 19:42:51 +0900

    mod

--- webapp/webserv.py
+++ webapp/webserv.py
@@ -1,9 +1,8 @@
 import Adafruit_DHT as dht
 from flask import Flask, render_template
-#from gpiozero
 
 app = Flask(__name__)
-# modify by Kimjs 22/06/22 17:38
+# modify by Kimjs 22/06/22 17:38 2 
 @app.route('/')
 def index():
     sensor = dht.DHT22
